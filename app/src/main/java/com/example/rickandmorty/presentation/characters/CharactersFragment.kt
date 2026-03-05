e31d7addb460b4eabe80083cccf429803e374b44
carloshi <carloshdevbr@gmail.com>
2026-03-04 22:32:34 -0300

    [wip] corrige importacoes

--- app/src/main/java/com/example/rickandmorty/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/characters/CharactersFragment.kt
@@ -7,9 +7,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import com.example.rickandmorty.databinding.FragmentCharactersBinding
 import com.example.rickandmorty.presentation.characters.adapter.CharactersAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
