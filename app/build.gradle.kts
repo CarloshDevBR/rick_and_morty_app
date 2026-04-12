3ce3ba5606a66ecde8adb9542983aa19b054d2b3
carloshi <carloshdevbr@gmail.com>
2026-04-12 00:11:22 -0300

    test: remove assinatura

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,16 +12,6 @@ android {
     namespace = "com.example.rickandmorty"
     compileSdk = 36
 
-    signingConfigs {
-        create("release") {
-            storeFile =
-                file("/Users/carloshi/StudioProjects/rick_and_morty_app/rick_and_morty_app_store.jks")
-            storePassword = "Pokmon0?"
-            keyAlias = "rick-and-morty-app"
-            keyPassword = "Pokmon0?"
-        }
-    }
-
     defaultConfig {
         applicationId = "com.example.rickandmorty"
         minSdk = 24
@@ -35,21 +25,9 @@ android {
         val publicKey = providers.gradleProperty("PUBLIC_KEY").get()
         val privateKey = providers.gradleProperty("PRIVATE_KEY").get()
 
-        buildConfigField(
-            "String",
-            "BASE_URL",
-            "\"$baseUrl\""
-        )
-        buildConfigField(
-            "String",
-            "PUBLIC_KEY",
-            "\"$publicKey\""
-        )
-        buildConfigField(
-            "String",
-            "PRIVATE_KEY",
-            "\"$privateKey\""
-        )
+        buildConfigField("String", "BASE_URL", "\"$baseUrl\"")
+        buildConfigField("String", "PUBLIC_KEY", "\"$publicKey\"")
+        buildConfigField("String", "PRIVATE_KEY", "\"$privateKey\"")
     }
 
     detekt {
@@ -84,7 +62,6 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("release")
         }
     }
     buildFeatures {
